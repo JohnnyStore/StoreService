b024b2a60e4e7a3b33a15adef485fefb81ac070e
yangyajing1026 <842080819@qq.com>
2017-12-20 22:26:59 +0800

    上传测试

--- src/main/java/com/johnny/store/controller/BrandController.java
+++ src/main/java/com/johnny/store/controller/BrandController.java
@@ -6,6 +6,9 @@ import com.johnny.store.service.impl.BrandServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 商品品牌yangyajing
+ */
 @RestController
 public class BrandController {
     @Autowired
